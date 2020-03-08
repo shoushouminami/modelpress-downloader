f514a71152dea4876496402fde4cc49e46f550fa
小小南 <shoushouminami@gmail.com>
2020-03-07 19:56:04 -0800

    Support entamenext.com

--- src/inject/entamenext.com.js
+++ src/inject/entamenext.com.js
@@ -1,10 +1,10 @@
 const utils = require("../utils.js");
 
-const getLargeImg = function (dom) {
+const getLargeImgFromDom = function (dom) {
     return getLargeImgFromUrl(utils.getDomBackgroundImage(dom));
 };
 
-const pattern = /^https?:\/\/images\.entamenext\.com\/articles_photos\/\d+\/\d+\/(\d+x\d+)\/.*\.jpg$/i;
+const pattern = /^https?:\/\/images\.entamenext\.com\/articles_photos\/\d+\/\d+\/(\d+x\d+)\/.*\.(jpg|png)$/i;
 const getLargeImgFromUrl = function (url) {
     if (url && url.match(pattern)) {
         return url.replace(url.match(pattern)[1], "ORG");
@@ -14,7 +14,10 @@ const getLargeImgFromUrl = function (url) {
 
 const inject = function () {
     let o = require("./return-message.js").init();
-    utils.pushArray(o.images, utils.findDomsWithCssSelector(document, "section div.articleGalleryList ul li p.img", getLargeImg));
+    utils.pushArray(o.images, utils.findDomsWithCssSelector(document, "div.cont section div.articleGalleryImg div.img", getLargeImgFromDom));
+    utils.pushArray(o.images, utils.findDomsWithCssSelector(document, "div.cont section div.articleGalleryList ul li p.img", getLargeImgFromDom));
+    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "div.cont section div.articleDetailImg div.img img", getLargeImgFromUrl));
+    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "div.cont section div.article_detail img", getLargeImgFromUrl));
     return o;
 };
 
@@ -22,7 +25,7 @@ module.exports = {
     inject: inject,
     host: "entamenext.com",
     test: {
-        getLargeImg: getLargeImg,
+        getLargeImgFromDom: getLargeImgFromDom,
         getLargeImgFromUrl: getLargeImgFromUrl,
         pattern: pattern
     }
--- test/inject/test.js
+++ test/inject/test.js
@@ -886,3 +886,25 @@ test("Test spice.eplus.jp article page with extra small images", async () => {
             }
         });
 });
+
+// https://entamenext.com/
+test("Test entamenext.com image page", async () => {
+    await testDirectDownload(
+        browser,
+        "https://entamenext.com/articles/gallery/4011/1",
+        "entamenext.com-articles-gallery-4011-1/",
+        ["https://images.entamenext.com/articles_photos/4000/4011/ORG/aef621b8d71c50e985c8872510939652.jpg",
+            "https://images.entamenext.com/articles_photos/4000/4011/ORG/ed55d3709ff988df9927b048182153f7.jpg",
+            "https://images.entamenext.com/articles_photos/4000/4011/ORG/e488f818267c15aadaeddec5218e45ac.jpg",
+            "https://images.entamenext.com/articles_photos/4000/4011/ORG/195144b32b0cb508ee3606138fd60233.jpg",
+            "https://images.entamenext.com/articles_photos/4000/4011/ORG/ce10836abde6e79ad985b59ef5f2b4e0.jpg",
+            "https://images.entamenext.com/articles_photos/4000/4011/ORG/dcc727986b3bacae2e8c27f92cf75957.jpg"]);
+});
+
+test("Test entamenext.com article page", async () => {
+    await testDirectDownload(
+        browser,
+        "https://entamenext.com/articles/detail/4011",
+        "entamenext.com-articles-detail-4011/",
+        ["https://images.entamenext.com/articles/4000/4011/ORG/7b97380f695ac9f13d5170d6ce47d144.jpg"]);
+});
