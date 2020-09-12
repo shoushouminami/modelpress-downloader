94a31323b9722671d33d6ee35c53d2122770906a
小小南 <shoushouminami@gmail.com>
2020-09-12 14:41:08 -0700

    unit test

--- test/inject/sites.test.js
+++ test/inject/sites.test.js
@@ -1,7 +1,7 @@
 const sites = require("../../src/inject/sites");
 
 test("Test sites.all", () => {
-    expect(sites.all().length).toBe(44);
+    expect(sites.all().length).toBe(46);
     let hosts = [];
     let urls = [];
     sites.all().forEach((module) => {
@@ -55,6 +55,8 @@ test("Test sites.all", () => {
     expect(hosts).toContain("www.crank-in.net");
     expect(hosts).toContain("mainichikirei.jp");
     expect(hosts).toContain("www.rbbtoday.com");
+    expect(hosts).toContain("www.tvlife.jp");
+    expect(hosts).toContain("www.m-on-music.jp");
     expect(urls).toContain("https://www.bilibili.com/read/home");
     expect(urls).toContain("https://mdpr.jp");
     expect(urls).toContain("https://thetv.jp");
