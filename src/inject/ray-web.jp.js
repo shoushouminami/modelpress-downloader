096b3f62a89b017d13b86f7680ce3630475891d1
小小南 <shoushouminami@gmail.com>
2020-09-30 22:44:32 -0700

    Fix ray-web.jp

--- src/inject/ray-web.jp.js
+++ src/inject/ray-web.jp.js
@@ -1,10 +1,25 @@
 const utils = require("../utils.js");
+const getLargeImgFromDom = function (img){
+    if (img.srcset) {
+        return getLargeImg(img.srcset.split(",")[0].split(" ")[0]);
+    }
+};
+
+const getLargeImg = function (url) {
+    if (url) {
+        return utils.filterTrailingResolutionNumbers(url);
+    }
+};
+
 const inject = function () {
     let o = require("./return-message.js").init();
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.current_photo img", utils.removeQuery));
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.thumb_list ul img", utils.removeQuery));
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.top_photo img", utils.removeQuery));
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.post_cont div.pic_media.scale_full img", utils.removeQuery));
+    // gallery page top image
+    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.current_photo img", getLargeImg));
+    // gallery page image list
+    utils.pushArray(o.images, utils.findDomsWithCssSelector(document, "main article#post div.thumb_list ul img", getLargeImgFromDom));
+
+    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.top_photo img", getLargeImg));
+    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "main article#post div.post_cont div.pic_media.scale_full img", getLargeImg));
     return o;
 };
 
