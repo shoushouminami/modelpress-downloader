b2727100b241cb76b36e565e7d978998c6c10aed
小小南 <shoushouminami@gmail.com>
2020-10-03 21:39:36 -0700

    fix unit test

--- test/inject/www.fujitv-view.jp.test.js
+++ test/inject/www.fujitv-view.jp.test.js
@@ -1,5 +1,4 @@
 const {testDirectDownload, getBrowserFactory} = require("./testbase");
-const pageutils = require("../pageutils");
 
 let browserFactory = getBrowserFactory(beforeAll, afterAll);
 
@@ -9,7 +8,12 @@ test("Test news article", async () => {
         "https://www.fujitv-view.jp/article/post-166141/",
         "www.fujitv-view.jp-article-post-166141-/",
         [
-            "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0015-1-scaled.jpg?resize=1536,864&crop=4px,58px,2556px,1433px",
+            {
+                "retries": [
+                    "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0015-1-scaled.jpg"
+                ],
+                "url": "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0015-1.jpg"
+            },
             {
                 "retries": [
                     "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0025-1-scaled.jpg"
@@ -48,17 +52,12 @@ test("Test news article", async () => {
             },
             {
                 "retries": [
-                    "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0015-1-scaled.jpg"
-                ],
-                "url": "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0015-1.jpg"
-            },
-            {
-                "retries": [
                     "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0065-scaled.jpg"
                 ],
                 "url": "https://www.fujitv-view.jp/tachyon/2020/09/DSC_0065.jpg"
             }
-        ]);
+        ]
+    );
 });
 
 test("Test image gallery", async () => {
