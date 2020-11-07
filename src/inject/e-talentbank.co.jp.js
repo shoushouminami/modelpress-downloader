de73d0920881784c336b56317ab78ef791d0b0f2
小小南 <shoushouminami@gmail.com>
2020-11-06 18:56:14 -0800

    Fix e-talentbank.co.jp

--- src/inject/e-talentbank.co.jp.js
+++ src/inject/e-talentbank.co.jp.js
@@ -1,12 +1,39 @@
 const utils = require("../utils.js");
 const getLargeImg = (url) => {
-    return utils.removeTrailingResolutionNumbers(utils.removeQuery(url));
+    return utils.removeDataUrl(utils.removeTrailingResolutionNumbers(utils.removeQuery(url)));
 };
 const inject = function () {
     let o = require("./return-message.js").init();
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "div#wrapper_in div.contents article div.post div.post_head div.thumb img", getLargeImg));
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "div#wrapper_in div.contents article div.post div.post_photos div.table p a img", getLargeImg));
-    utils.pushArray(o.images, utils.findImagesWithCssSelector(document, "div#wrapper_in div.contents article div.post div.post_body div.image p a img", getLargeImg));
+    utils.pushArray(
+        o.images,
+        utils.findImagesWithCssSelector(
+            document,
+            [
+                // main post image
+                "main .etb-single--eyecatchThumb img",
+                // main image and thumbnail
+                "main .etb-single--eyecatch .etb-single--eyecatchThumbs img",
+                // article end thumbnails
+                // "main .etb-single--relationphoto a div img",
+            ].join(","),
+            getLargeImg
+        )
+    );
+
+    if (document.querySelector(".etb-container--photo") == null) {
+        // news page
+        utils.pushArray(
+            o.images,
+            utils.findImagesWithCssSelector(
+                document,
+                [
+                    "main .etb-single--relationphoto a div img",
+                ].join(","),
+                getLargeImg
+            )
+        );
+    }
+
     return o;
 };
 
--- test/inject/e-talentbank.co.jp.test.js
+++ test/inject/e-talentbank.co.jp.test.js
@@ -9,7 +9,7 @@ afterAll(async () => {
     await browser.close();
 });
 
-test.skip("Test e-talentbank.co.jp article page with 1 image", async () => {
+test("Test e-talentbank.co.jp article page with 1 image", async () => {
     await testDirectDownload(
         browser,
         "https://e-talentbank.co.jp/news/139105/",
@@ -17,7 +17,7 @@ test.skip("Test e-talentbank.co.jp article page with 1 image", async () => {
         ["https://e-talentbank.co.jp/wp-content/uploads/2020/04/b0e9871e81145fe31bbc09d53c5e5aa4-2-680x680-1-1.jpg"]);
 });
 
-test.skip("Test e-talentbank.co.jp article page with 5 images", async () => {
+test("Test e-talentbank.co.jp article page with 5 images", async () => {
     await testDirectDownload(
         browser,
         "https://e-talentbank.co.jp/news/139166/",
@@ -29,11 +29,11 @@ test.skip("Test e-talentbank.co.jp article page with 5 images", async () => {
             "https://e-talentbank.co.jp/wp-content/uploads/2020/04/58c92516ee5b8f966347b1f91c71da5d.png"]);
 });
 
-test.skip("Test e-talentbank.co.jp attachment page", async () => {
+test("Test e-talentbank.co.jp attachment page", async () => {
     await testDirectDownload(
         browser,
-        "https://e-talentbank.co.jp/news/139166/attachment/%e3%82%b3%e3%83%a1%e3%83%b3%e3%83%88-2020-04-03-183112/",
-        "e-talentbank.co.jp-news-139166-attachment-%e3%82%b3%e3%83%a1%e3%83%b3%e3%83%88-2020-04-03-183112-/",
+        "https://e-talentbank.co.jp/photo/139226/",
+        "e-talentbank.co.jp-photo-139226-/",
         ["https://e-talentbank.co.jp/wp-content/uploads/2020/04/92d99a795a9ba4d3f863b2fe12c81977.png",
             "https://e-talentbank.co.jp/wp-content/uploads/2020/04/351ad2e3814a72c85844eafbf18eea6e.png",
             "https://e-talentbank.co.jp/wp-content/uploads/2020/04/56baebe0b634cf388978712e76439585.png",
@@ -41,24 +41,26 @@ test.skip("Test e-talentbank.co.jp attachment page", async () => {
             "https://e-talentbank.co.jp/wp-content/uploads/2020/04/58c92516ee5b8f966347b1f91c71da5d.png"]);
 });
 
-test.skip("Test e-talentbank.co.jp interview page", async () => {
+test("Test e-talentbank.co.jp interview attachment page", async () => {
     await testDirectDownload(
         browser,
-        "https://e-talentbank.co.jp/interview/96270/",
-        "e-talentbank.co.jp-interview-96270-/",
+        "https://e-talentbank.co.jp/interview/96270/attachment/d75_4804-2/",
+        "e-talentbank.co.jp-photo-96275-/",
         ["https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4804-1.jpeg",
             "https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4713-1.jpeg",
             "https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4799-1.jpeg",
             "https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4743-1.jpeg"]);
 });
 
-test.skip("Test e-talentbank.co.jp interview attachment page", async () => {
+test("Test news page with 3 photos ", async () => {
     await testDirectDownload(
         browser,
-        "https://e-talentbank.co.jp/interview/96270/attachment/d75_4804-2/",
-        "e-talentbank.co.jp-interview-96270-attachment-d75_4804-2-/",
-        ["https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4804-1.jpeg",
-            "https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4713-1.jpeg",
-            "https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4799-1.jpeg",
-            "https://e-talentbank.co.jp/wp-content/uploads/2019/05/D75_4743-1.jpeg"]);
+        "https://e-talentbank.co.jp/news/175388/",
+        "e-talentbank.co.jp-news-175388-/",
+        [
+            "https://etb-media-photo.s3.ap-northeast-1.amazonaws.com/wp-content/uploads/2020/11/05130058/aa8db53c03e6254a9d259fc7cb3976fc-680x474.jpg",
+            "https://etb-media-photo.s3.ap-northeast-1.amazonaws.com/wp-content/uploads/2020/11/05125924/%E3%83%A1%E3%82%A4%E3%83%B3%E3%82%A2%E3%83%BC%E5%86%99-1.jpg",
+            "https://etb-media-photo.s3.ap-northeast-1.amazonaws.com/wp-content/uploads/2020/11/05125838/%E3%83%A1%E3%82%A4%E3%83%B3%E3%82%A2%E3%83%BC%E5%86%99.jpg"
+        ]
+    );
 });
