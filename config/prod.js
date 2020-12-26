e6fd40b1a4759a03b3f52a4acd396e785ea670cb
小小南 <shoushouminami@gmail.com>
2020-12-26 01:38:17 -0800

    fix test

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    ga_property: "UA-156562835-1"
+    ga_property: "UA-156562835-1",
+    is_dev: false
 };
--- src/is-dev.js
+++ src/is-dev.js
@@ -1,1 +1,11 @@
-module.exports = __IS_DEV__;
+let isDev;
+
+if (typeof __IS_DEV__ === 'undefined') {
+    console.warn("isDev: In NodeJS build.");
+    isDev = global.isDev || false; // nodejs
+    console.log("nodejs: isDev: ", isDev);
+} else {
+    isDev = __IS_DEV__; //webpack
+    console.log("webpack: isDev: ", isDev);
+}
+module.exports = isDev;
