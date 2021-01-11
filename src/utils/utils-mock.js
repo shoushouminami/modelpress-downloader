8c3573d4f8533fb99a7ba7f291b38720b5b424b4
小小南 <shoushouminami@gmail.com>
2021-01-11 00:08:38 -0800

    Use smartphone emoji in remote image status

--- src/utils/utils-mock.js
+++ src/utils/utils-mock.js
@@ -2,8 +2,8 @@ const utils = require("../utils");
 
 export function fetchUrl(url) {
     return new Promise(function (resolve, reject) {
-        resolve(JSON.stringify(require("./mdpr-dummy-response")));
-        // reject(404, "bad 500");
+        // resolve(JSON.stringify(require("./mdpr-dummy-response")));
+        reject(500, "bad 500");
     });
 }
 
