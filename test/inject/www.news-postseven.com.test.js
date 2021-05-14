2013dea66b67d8c266438e21fa5d8801e9c1743f
小小南 <shoushouminami@gmail.com>
2021-05-14 01:10:20 -0700

    fix tests

--- test/inject/www.news-postseven.com.test.js
+++ test/inject/www.news-postseven.com.test.js
@@ -1,26 +1,32 @@
 const {testDirectDownload, getBrowserFactory} = require("./testbase");
+const retries = require("../utils/retries");
 let getBrowser = getBrowserFactory(beforeAll, afterAll);
 
 test("Test news article image gallery page", async () => {
-    await testDirectDownload(
-        getBrowser(),
-        "https://www.news-postseven.com/archives/20200813_1584763.html?DETAIL&IMAGE&PAGE=1",
-        "www.news-postseven.com-archives-20200813_1584763.html/",
-        [
+    retries(3, async () => {
+        await testDirectDownload(
+            getBrowser(),
+            "https://www.news-postseven.com/archives/20200813_1584763.html?DETAIL&IMAGE&PAGE=1",
+            "www.news-postseven.com-archives-20200813_1584763.html/",
+            [
                 "https://www.news-postseven.com/uploads/2020/08/05/nishino_nanase_01.jpg",
                 "https://www.news-postseven.com/uploads/2020/08/05/nishino_nanase_02.jpg",
                 "https://www.news-postseven.com/uploads/2020/08/13/nanase_nishino_photobook01.jpg"
-        ]);
+            ]);
+    });
+
 });
 
 test("Test news article detail page", async () => {
+    retries(3, async () => {
         await testDirectDownload(
             getBrowser(),
             "https://www.news-postseven.com/archives/20200813_1584763.html?DETAIL",
             "www.news-postseven.com-archives-20200813_1584763.html/",
             [
-                    "https://www.news-postseven.com/uploads/2020/08/05/nishino_nanase_01.jpg",
-                    "https://www.news-postseven.com/uploads/2020/08/05/nishino_nanase_02.jpg",
-                    "https://www.news-postseven.com/uploads/2020/08/13/nanase_nishino_photobook01.jpg"
+                "https://www.news-postseven.com/uploads/2020/08/05/nishino_nanase_01.jpg",
+                "https://www.news-postseven.com/uploads/2020/08/05/nishino_nanase_02.jpg",
+                "https://www.news-postseven.com/uploads/2020/08/13/nanase_nishino_photobook01.jpg"
             ]);
+    });
 });
