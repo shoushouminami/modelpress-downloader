9753455f9c2c59a5d4d94a8a3ced144383ce0b85
小小南 <shoushouminami@gmail.com>
2021-08-22 17:48:54 -0700

    fix tests

--- test/inject/bltweb.jp.test.js
+++ test/inject/bltweb.jp.test.js
@@ -16,16 +16,16 @@ test("Test bltweb.jp page", async () => {
         "https://bltweb.jp/2019/12/10/blt2020newyear_n46_3-4_store/",
         "bltweb.jp-2019-12-10-blt2020newyear_n46_3-4_store-/",
         [
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_724,h_1024/https://bltweb.jp/wp/wp-content/uploads/2019/12/20191215_BLT2020-02_NEWYEAR_H1_STORE.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_422,h_598/https://bltweb.jp/wp/wp-content/uploads/2019/12/d65a7000af282857e9caa178de0ea148.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_600,h_424/https://bltweb.jp/wp/wp-content/uploads/2019/12/8751914e48f26781804ae4ea5569ec3f.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_643,h_900/https://bltweb.jp/wp/wp-content/uploads/2019/12/BLT2020-02_3rd-4th_SPECIAL-BOOK-1.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_406,h_601/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_kubo.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_406,h_601/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_yoda.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_405,h_599/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_endo.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_405,h_599/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_kaki.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_405,h_599/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-hayasi.jpg",
-            "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_406,h_601/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-ishida.jpg"
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_724,h_1024/https://bltweb.jp/wp/wp-content/uploads/2019/12/20191215_BLT2020-02_NEWYEAR_H1_STORE.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_422,h_598/https://bltweb.jp/wp/wp-content/uploads/2019/12/d65a7000af282857e9caa178de0ea148.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_600,h_424/https://bltweb.jp/wp/wp-content/uploads/2019/12/8751914e48f26781804ae4ea5569ec3f.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_643,h_900/https://bltweb.jp/wp/wp-content/uploads/2019/12/BLT2020-02_3rd-4th_SPECIAL-BOOK-1.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_406,h_601/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_kubo.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_406,h_601/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_yoda.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_405,h_599/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_endo.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_405,h_599/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-N46_kaki.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_405,h_599/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-hayasi.jpg",
+            "https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_406,h_601/https://bltweb.jp/wp/wp-content/uploads/2019/12/4L-ishida.jpg"
         ]
     );
 });
--- test/inject/www.comic-valkyrie.com.test.js
+++ test/inject/www.comic-valkyrie.com.test.js
@@ -4,107 +4,122 @@ const getBrowser = getBrowserFactory(beforeAll, afterAll);
 test("Test samplebook page", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/",
-        "www.comic-valkyrie.com-samplebook-val_seidonnoryu07-/",
+        "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/",
+        "www.comic-valkyrie.com-samplebook-val_bakesyo18-/",
         [
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0010.jpg",
-                "filename": "0010.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0012.jpg",
-                "filename": "0012.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0014.jpg",
-                "filename": "0014.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0008.jpg",
-                "filename": "0008.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0006.jpg",
-                "filename": "0006.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0004.jpg",
-                "filename": "0004.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0002.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0002.jpg",
                 "filename": "0002.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0013.jpg",
-                "filename": "0013.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0011.jpg",
-                "filename": "0011.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0009.jpg",
-                "filename": "0009.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0007.jpg",
-                "filename": "0007.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0005.jpg",
-                "filename": "0005.jpg",
-                "type": "msg"
-            },
-            {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0003.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0003.jpg",
                 "filename": "0003.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0001.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0004.jpg",
+                "filename": "0004.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0005.jpg",
+                "filename": "0005.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0001.jpg",
                 "filename": "0001.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0015.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0006.jpg",
+                "filename": "0006.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0007.jpg",
+                "filename": "0007.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0008.jpg",
+                "filename": "0008.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0009.jpg",
+                "filename": "0009.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0010.jpg",
+                "filename": "0010.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0011.jpg",
+                "filename": "0011.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0012.jpg",
+                "filename": "0012.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0013.jpg",
+                "filename": "0013.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0014.jpg",
+                "filename": "0014.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0015.jpg",
                 "filename": "0015.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0016.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0016.jpg",
                 "filename": "0016.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0017.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0017.jpg",
                 "filename": "0017.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0018.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0018.jpg",
                 "filename": "0018.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0019.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0019.jpg",
                 "filename": "0019.jpg",
                 "type": "msg"
             },
             {
-                "url": "https://www.comic-valkyrie.com/samplebook/val_seidonnoryu07/data/0020.jpg",
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0020.jpg",
                 "filename": "0020.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0021.jpg",
+                "filename": "0021.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0022.jpg",
+                "filename": "0022.jpg",
+                "type": "msg"
+            },
+            {
+                "url": "https://www.comic-valkyrie.com/samplebook/val_bakesyo18/data/0023.jpg",
+                "filename": "0023.jpg",
                 "type": "msg"
             }
         ]
