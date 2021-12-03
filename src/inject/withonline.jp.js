1f7e8d3dcbdb8fc81f7070a9d4d3c38e9ce8a79a
小小南 <shoushouminami@gmail.com>
2021-12-03 14:30:42 -0800

    download original images on withonline.jp

--- src/inject/withonline.jp.js
+++ src/inject/withonline.jp.js
@@ -1,4 +1,5 @@
 const utils = require("../utils.js");
+const re = /^.*(\/(large|medium|small)\/).*$/;
 module.exports = {
     inject: function () {
         let o = require("./return-message.js").init();
@@ -11,10 +12,16 @@ module.exports = {
             "#contents .contents-btm .post-contents .pattern_1 img", //lifestyle article image
             "#contents .contents-btm .post-contents .pattern_2 img", //lifestyle article image
             "#contents .contents-btm .post-contents .slider-container .slide-img img",
+            "#post .photo_gallery .thumb_list .img-box img", // thumbnail page
         ]) {
             utils.pushArray(o.images,
-                utils.findLazyImagesWithCssSelector(document, selector, function (src){
-                    return utils.removeQuery(src);
+                utils.findLazyImagesWithCssSelector(document, selector, function (url){
+                    url = utils.removeQuery(url);
+                    let m = url.match(re);
+                    if (m) {
+                        return url.replace(m[1], "/original/");
+                    }
+                    return url;
                 })
             );
         }
