38b2e117d0cbd0d176a868f88f8ed9dc7bbbedb6
小小南 <shoushouminami@gmail.com>
2022-02-14 19:35:00 -0800

    fix tests

--- test/inject/www.mensnonno.jp.test.js
+++ test/inject/www.mensnonno.jp.test.js
@@ -4,13 +4,14 @@ let getBrowser = getBrowserFactory(beforeAll, afterAll);
 test("Test fashion article", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://www.mensnonno.jp/post/146442/",
-        "www.mensnonno.jp-post-146442-/",
+        "https://www.mensnonno.jp/post/140283/",
+        "www.mensnonno.jp-post-140283-/",
         [
-            "https://www.mensnonno.jp/wp-content/uploads/2021/12/MN2201-02-AsukaSaito-eyecatch-2.jpg",
-            "https://www.mensnonno.jp/wp-content/uploads/2020/10/asuka_2her_title_03.jpg",
-            "https://www.mensnonno.jp/wp-content/uploads/2021/12/MN2201-02-AsukaSaito-m1.jpg",
-            "https://www.mensnonno.jp/wp-content/uploads/2021/12/MN2201-02-AsukaSaito-m2.jpg"
+            "https://www.mensnonno.jp/wp-content/uploads/2021/11/2714b7856e44332930d33d530f391ba0.jpg",
+            "https://www.mensnonno.jp/wp-content/uploads/2021/11/1e23e5360752a3ee7ff06f738daa1f74.png",
+            "https://www.mensnonno.jp/wp-content/uploads/2021/11/fd96127e80d1ad27dbc467dd384f93cd.png",
+            "https://www.mensnonno.jp/wp-content/uploads/2021/11/09b4c1ae3d778f706ea00b6868fcd77f.png",
+            "https://www.mensnonno.jp/wp-content/uploads/2021/11/4685bc2da199d385898d0bac2f4d7509.png"
         ]
     );
 });
