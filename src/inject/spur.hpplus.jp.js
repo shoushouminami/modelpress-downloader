3c8f5984592ca5cf977337eb00c2ca8de1c15c4f
小小南 <shoushouminami@gmail.com>
2022-08-29 18:46:58 -0700

    remove spur.hpplus.jp

--- src/inject/spur.hpplus.jp.js
+++ src/inject/spur.hpplus.jp.js
@@ -2,41 +2,7 @@ const utils = require("../utils.js");
 
 module.exports = {
     inject: function () {
-        let o = require("./return-message.js").init();
-        // special top image
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(
-                document,
-                "#mainContent .main-area .main-image-area img")
-        );
-
-        // article images
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(
-                document,
-                "#mainContent .main-area .txt-related img")
-        );
-
-        //special page images
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(
-                document,
-                [
-                    "main section .style-inner__head img",
-                    "main section .style-img img"
-                ].join(","))
-        );
-
-        //magazine page
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(
-                document,
-                [
-                    "#mainContent #preview li img",
-                ].join(","))
-        );
-
-        return o;
+       return require("./return-message.js").notSupported()
     },
     host: "spur.hpplus.jp",
 };
