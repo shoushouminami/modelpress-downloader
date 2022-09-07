88aa2e075fed2a4d8c9e118a2b704dfafe22594c
Linda Huang <hhuang1206@gmail.com>
2022-09-07 00:13:36 -0700

    Add eiga.com

---
+++ src/inject/eiga.com.js
@@ -0,0 +1,32 @@
+const utils = require("../utils.js");
+
+function getLargeImg(url) {
+    if (url == null) {
+        return null;
+    }
+
+    if (/\/\d+\.jpg/g.test(url)) {
+        url = url.replace(/\/\d+\.jpg/g, ".jpg");
+    }
+
+    return utils.removeQuery(url);
+}
+
+const inject = function () {
+    let o = require("./return-message.js").init();
+    for (const selector of [
+        ".content-main .article-image img", // article image
+    ]) {
+        utils.pushArray(
+            o.images,
+            utils.findLazyImagesWithCssSelector(document, selector, getLargeImg)
+        );
+    }
+
+    return o;
+};
+
+module.exports = {
+    inject: inject,
+    host: "eiga.com",
+};
