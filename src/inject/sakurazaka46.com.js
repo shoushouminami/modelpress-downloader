f9fded3a31b01d87a7373abc5d3972f415f05bbe
小小南 <shoushouminami@gmail.com>
2022-10-20 16:34:16 -1000

    Add FC content on sakurazaka46.com

--- src/inject/sakurazaka46.com.js
+++ src/inject/sakurazaka46.com.js
@@ -1,13 +1,23 @@
-const utils = require("../utils.js");
-const filterIcon = function(url) {
+const utils = require("../utils");
+const globals = require("../globals");
+function filterIcon(url) {
     if (url.indexOf("twemoji.maxcdn.com") > -1) {
         return null;
     }
-
     return url;
-};
+}
+
+const re = /.*(\/\d{3}_\d{3}_\d{6})\.(jpg|jpeg)$/i ;
+function getLargeImg(url) {
+    if (url.match(re)) {
+        url = url.replace(url.match(re)[1], "");
+    }
+    return url;
+}
+
 module.exports = {
     inject: function () {
+        // Member blog
         let o = require("./return-message.js").init();
         utils.pushArray(
             o.images,
@@ -17,7 +27,41 @@ module.exports = {
                 filterIcon
             )
         );
+        // FC history images
+        utils.pushArray(
+            o.images,
+            utils.findDomsWithCssSelector(
+                document,
+                "main .sakura-history-detail-list .c-item .c-thumb-img img",
+                function (dom) {
+                    let url = utils.getDomBackgroundImage(dom);
+                    if (url == null) {
+                        return null;
+                    }
+                    if (url.startsWith("/")) {
+                        url = window.origin + url;
+                    }
+                    return getLargeImg(url);
+                }
+            )
+        );
+        // FC manager diary
+        utils.pushArray(
+            o.images,
+            utils.findImagesWithCssSelector(
+                document,
+                "main .manager-list dl dd div li img",
+                function (url) {
+                    return getLargeImg(url);
+                }
+            )
+        );
+        let ct= globals.getSearchParamValue("ct");
+        if (ct) {
+            o.folder = o.folder.substring(0, o.folder.length - 1) + "-" + ct + "/";
+        }
         return o;
     },
-    host: "sakurazaka46.com"
+    host: "sakurazaka46.com",
+    re: re
 };
--- test/inject/sakurazaka46.com.test.js
+++ test/inject/sakurazaka46.com.test.js
@@ -1,5 +1,5 @@
 const {testDirectDownload, getBrowserFactory} = require("./testbase");
-
+const {re} = require("../../src/inject/sakurazaka46.com");
 let getBrowser = getBrowserFactory(beforeAll, afterAll);
 
 test("Test keyakizaka46 blog article", async () => {
@@ -12,4 +12,12 @@ test("Test keyakizaka46 blog article", async () => {
             "https://sakurazaka46.com/files/14/diary/s46/blog/moblog/202010/mobNBUY1d.jpg"
         ]
     );
+});
+
+test("Test regex", async () => {
+    const re = require("../../src/inject/sakurazaka46.com").re;
+    expect("/images/14/f3c/27fd9324d73bd54de502c1a3e1796/960_960_102400.jpg".match(re)).toBeTruthy()
+    expect("/images/14/f3c/27fd9324d73bd54de502c1a3e1796/960_960_102400.jpg".match(re)[1]).toBe("/960_960_102400")
+    expect("/images/14/f3c/27fd9324d73bd54de502c1a3e1796/300_300_102400.jpg".match(re)).toBeTruthy()
+    expect("/images/14/f3c/27fd9324d73bd54de502c1a3e1796/300_300_102400.jpg".match(re)[1]).toBe("/300_300_102400")
 });
