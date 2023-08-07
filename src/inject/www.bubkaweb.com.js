9dbc3c8b2cfc223f6202832209f4d4acf9bda0e8
熊猫 <shoushouminami@gmail.com>
2023-08-06 22:49:37 -0700

    add www.bubkaweb.com

---
+++ src/inject/www.bubkaweb.com.js
@@ -0,0 +1,22 @@
+const utils = require("../utils.js");
+
+module.exports = {
+    inject: function () {
+        let o = require("./return-message.js").init();
+        for (let selector of [
+            "#main .post .wp-block-image img", // article top image
+            "#main .post .post_attachment_mainimg figure img", // article top image
+            "#main .post .post_attachment_thumbnail li img", // article top image
+        ]) {
+            utils.pushArray(o.images,
+                utils.findLazyImagesWithCssSelector(
+                    document,
+                    selector,
+                    utils.removeTrailingResolutionNumbers
+                )
+            );
+        }
+        return o;
+    },
+    host: "www.bubkaweb.com"
+};
