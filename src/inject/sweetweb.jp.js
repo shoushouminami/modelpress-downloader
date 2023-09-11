e7dd3df9d4eef614bee7273acd222c4dd8149532
熊猫 <shoushouminami@gmail.com>
2023-09-10 22:34:51 -0700

    fix sweetweb.jp

--- src/inject/sweetweb.jp.js
+++ src/inject/sweetweb.jp.js
@@ -2,8 +2,7 @@ const utils = require("../utils.js");
 const inject = function() {
     let o = require("./return-message.js").init();
     for (const selector of [
-        "#contents #column .column-head .image img", // top image
-        "#contents #column .column-content p img", // top image
+        "main .contents .post .postBody img", // top image
     ]) {
         utils.pushArray(o.images,
             utils.findLazyImagesWithCssSelector(
