0c7c48972d7fc3ab399a5ed32f140c0efe712f6c
熊猫 <shoushouminami@gmail.com>
2023-09-22 17:40:15 -0700

    fix tests

--- test/inject/blog.nogizaka46.com.test.js
+++ test/inject/blog.nogizaka46.com.test.js
@@ -2,16 +2,6 @@ const {testDirectDownload, getBrowserFactory} = require("./testbase");
 const {replaceIllegalChars} = require("../../src/inject/www.nogizaka46.com")
 const getBrowser = getBrowserFactory(beforeAll, afterAll);
 
-test("s/n46/diary/detail/65304?ima=0131", async () => {
-    await testDirectDownload(
-        getBrowser(),
-        "https://www.nogizaka46.com/s/n46/diary/detail/65304?ima=0131",
-        "早川聖来-2022.02.14_18.36-ハッピーバレンタイン　　早川聖来/",
-        [
-            "https://www.nogizaka46.com/files/46/diary/n46/MEMBER/0000%20%281%29_2.jpeg"
-        ]);
-});
-
 test("s/n46/diary/detail/100484?ima=1429&cd=MEMBER", async () => {
     await testDirectDownload(
         getBrowser(),
