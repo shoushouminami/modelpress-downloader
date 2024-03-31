d17841920ca49ccd28e9d94ce4239fba18c84504
熊猫 <shoushouminami@gmail.com>
2024-03-31 23:19:54 +0900

    update www.nhk.or.jp

--- src/inject/www.nhk.or.jp.js
+++ src/inject/www.nhk.or.jp.js
@@ -1,14 +1,41 @@
 const utils = require("../utils.js");
 const inject = function() {
     let o = require("./return-message.js").init();
-    for (const selector of [
-        "#main #center .entry:nth-child(2) img",
-        // "#main #center div div > img",
-    ]) {
+    // for (const selector of [
+    //     "#main #center .entry:nth-child(1) img",
+    //     // "#main #center div div > img",
+    // ]) {
+    //     utils.pushArray(o.images,
+    //         utils.findLazyImagesWithCssSelector(
+    //             document,
+    //             selector,
+    //             function (url) {
+    //                 if (!(url.endsWith(".jpg") || url.endsWith(".JPG")
+    //                     || url.endsWith(".jpeg") || url.endsWith(".JPEG"))) {
+    //                     return null;
+    //                 }
+    //                 return url;
+    //             }
+    //         )
+    //     );
+    // }
+    const divDOMs = utils.findDomsWithCssSelector(document, "#main #center div.entry", function (divDOM) {
+        const h2Divs = utils.findDomsWithCssSelector(divDOM, "h2", function (h2Div) {
+            if (h2Div.innerText.indexOf("らじらー！サンデー") > -1) {
+                return h2Div;
+            }
+        });
+
+        if (h2Divs.length > 0) {
+            return divDOM;
+        }
+    });
+
+    if (divDOMs.length > 0) {
         utils.pushArray(o.images,
             utils.findLazyImagesWithCssSelector(
-                document,
-                selector,
+                divDOMs[0],
+                "img",
                 function (url) {
                     if (!(url.endsWith(".jpg") || url.endsWith(".JPG")
                         || url.endsWith(".jpeg") || url.endsWith(".JPEG"))) {
