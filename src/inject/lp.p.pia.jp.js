2edd2af09a2cbebf855b12c3be9cd3a2b6564800
熊猫 <shoushouminami@gmail.com>
2024-06-10 12:51:46 +0900

    fix lp.p.pia.jp

--- src/inject/lp.p.pia.jp.js
+++ src/inject/lp.p.pia.jp.js
@@ -1,28 +1,42 @@
 const utils = require("../utils.js");
 const getLargeImg = function (url) {
-    if (url.endsWith(".JPG")) {
-        return url.replace(".JPG", ".jpg");
+    if (url) {
+        url = url.split("\")")[0];
+        if (url.startsWith("/")) {
+            url = window.location.origin + url;
+        }
+
+        if (url.endsWith("/thumbnail")) {
+            url = url.replace("/thumbnail", "/origin.jpg");
+        }
+
+        if (url.endsWith(".JPG")) {
+            return url.replace(".JPG", ".jpg");
+        }
     }
-    
+
     return url;
 }
 
 module.exports = {
     inject: function () {
         let o = require("./return-message.js").init();
-        // special top image
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(
-                document,
-                [
-                    // article top image
-                    "#mainContents .md-mainVisual img",
-                    // article images
-                    "#mainContents .md-thumbnailList img"
-                ].join(","),
-                getLargeImg
-            )
-        );
+        for (const selector of [
+            // article top image
+            "#mainContents .md-mainVisual img", // special top image
+            // article images
+            "#mainContents .md-thumbnailList img",
+            "#mainContents .photoGallaryArea img.photoGallaryArea__largeImage", // gallery top image
+            "#mainContents .photoGallaryArea__list img",
+            ".swiper-wrapper .swiper-slide .imageWrapper img", // slide show gallery
+        ]) {
+            utils.pushArray(o.images,
+                utils.findLazyImagesWithCssSelector(
+                    document,
+                    selector,
+                    getLargeImg)
+            );
+        }
 
         // gallery images
         utils.pushArray(o.images,
@@ -31,20 +45,7 @@ module.exports = {
                 "#mainContents .md-gallery .md-gallery__thumbnail a",
                 function (dom) {
                     if (dom && dom.style && dom.style.backgroundImage) {
-                        let url = dom.style.backgroundImage.split("url(\"")[1];
-                        if (url) {
-                            url = url.split("\")")[0];
-                            if (url.startsWith("/")) {
-                                url = window.location.origin + url;   
-                            }
-
-                            if (url.endsWith("/thumbnail")) {
-                                url = url.replace("/thumbnail", "/origin.jpg");
-                            }
-
-                            return url;
-                        }
-                        
+                        return getLargeImg(dom.style.backgroundImage.split("url(\"")[1])
                     }
                 })
         );
