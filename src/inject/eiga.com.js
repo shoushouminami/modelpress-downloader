cfc0837bede70d6eb1ee160486390d16992dd356
熊猫 <shoushouminami@gmail.com>
2024-10-05 22:45:22 -1000

    fix eiga.com

--- src/inject/eiga.com.js
+++ src/inject/eiga.com.js
@@ -16,7 +16,11 @@ const inject = function () {
     let o = require("./return-message.js").init();
     for (const selector of [
         ".content-main .article-image img", // article image
-        ".content-main .slide-show .slick-slide img"
+        ".content-main .slide-show .slick-slide img",
+        ".main-column .article-wrap figure img", // article image
+        ".article-wrap .gallery-wrap .swiper-slide img", // article slide
+        ".article-wrap .article-image img", // photo page main
+        ".article-wrap .article-image .thumbnail img", // photo page main
     ]) {
         utils.pushArray(
             o.images,
