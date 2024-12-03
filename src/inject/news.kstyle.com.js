c6c8a56755c23f7f16bde6abfc84389dc58f0885
熊猫 <shoushouminami@gmail.com>
2024-12-02 14:26:15 -1000

    fix on kstyle.com

--- src/inject/news.kstyle.com.js
+++ src/inject/news.kstyle.com.js
@@ -4,20 +4,24 @@ module.exports = {
     inject: function () {
         let o = require("./return-message.js").init();
         // special top image
-        utils.pushArray(o.images,
-            utils.findLazyImagesWithCssSelector(
-                document,
-                [
-                    "article div[class$='Body'] img", // article images
-                ].join(","),
-                function (url) {
-                    let arr = url.split("/");
-                    if (arr.pop().startsWith("r.")) {
-                        return arr.join("/");
+        for (const selector of [
+            "article div[class$='Body'] img", // article images
+            "main div[class*='ArticleBody_body'] img",
+        ]) {
+            utils.pushArray(o.images,
+                utils.findLazyImagesWithCssSelector(
+                    document,
+                    selector,
+                    function (url) {
+                        let arr = url.split("/");
+                        if (arr.pop().startsWith("r.")) {
+                            return arr.join("/");
+                        }
+                        return url;
                     }
-                    return url;
-                })
-        );
+                )
+            );
+        }
 
         const articleNo = globals.getSearchParamValue("articleNo");
         if (articleNo){
@@ -27,6 +31,6 @@ module.exports = {
         return o;
     },
     host: "news.kstyle.com",
-    altHosts: ["www.kstyle.com"]
+    altHosts: ["www.kstyle.com", "kstyle.com"]
 };
 
--- test/inject/news.kstyle.com.test.js
+++ test/inject/news.kstyle.com.test.js
@@ -1,32 +1,62 @@
-const {testDirectDownload, getBrowserFactory} = require("./testbase");
+const {testDirectDownload, getBrowserFactory, dummyItems} = require("./testbase");
 let getBrowser = getBrowserFactory(beforeAll, afterAll);
 
 test("article.ksn?articleNo=2219259&categoryCode=ET", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://news.kstyle.com/article.ksn?articleNo=2219259&categoryCode=ET",
-        "news.kstyle.com-article.ksn-articleNo-2219259/",
+        "https://kstyle.com/article.ksn?articleNo=2219259&categoryCode=ET",
+        "kstyle.com-article.ksn-articleNo-2219259/",
         [
-            "https://cdn.livedoor.jp/kstyle/300132573e7c7de9992be2d1b91a7e5a.jpg",
-            "https://cdn.livedoor.jp/kstyle/13e390cf20947168dff2576946e0e39b.jpg",
-            "https://cdn.livedoor.jp/kstyle/9e52c9592f9c714daae46d7495352f53.jpg",
-            "https://cdn.livedoor.jp/kstyle/9524a3cab0482ce8ee439d20beb7215b.jpg",
-            "https://cdn.livedoor.jp/kstyle/ec7a62b88d91b2e299b0621d33a5e687.jpg"
+            "https://cdn.livedoor.jp/kstyle/kstyle/300132573e7c7de9992be2d1b91a7e5a.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/13e390cf20947168dff2576946e0e39b.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/9e52c9592f9c714daae46d7495352f53.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/9524a3cab0482ce8ee439d20beb7215b.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/ec7a62b88d91b2e299b0621d33a5e687.jpg"
         ]);
 });
 
 test("article.ksn?articleNo=2218169&categoryCode=ET", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://news.kstyle.com/article.ksn?articleNo=2218169&categoryCode=ET",
-        "news.kstyle.com-article.ksn-articleNo-2218169/",
+        "https://kstyle.com/article.ksn?articleNo=2218169&categoryCode=ET",
+        "kstyle.com-article.ksn-articleNo-2218169/",
         [
-            "https://cdn.livedoor.jp/kstyle/ffb07b0856ab990699b24a51ed2c71ee.jpg",
-            "https://cdn.livedoor.jp/kstyle/f492f0053a613965a6002e4fb4ad60cd.jpg",
-            "https://cdn.livedoor.jp/kstyle/47866c849fa0020f921a8189c9a5c1e1.jpg",
-            "https://cdn.livedoor.jp/kstyle/1e5795f578c0be9a6f9e17fbe54d69e1.jpg",
-            "https://cdn.livedoor.jp/kstyle/027b6c7c8aad3d4a0f7d13ac1e3c1f18.jpg",
-            "https://cdn.livedoor.jp/kstyle/cfba22703aaf79956d6efbb211ad05df.jpg",
-            "https://cdn.livedoor.jp/kstyle/e419f7861fe86424820f8e7ac4261b83.jpg"
+            "https://cdn.livedoor.jp/kstyle/kstyle/ffb07b0856ab990699b24a51ed2c71ee.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/f492f0053a613965a6002e4fb4ad60cd.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/47866c849fa0020f921a8189c9a5c1e1.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/1e5795f578c0be9a6f9e17fbe54d69e1.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/027b6c7c8aad3d4a0f7d13ac1e3c1f18.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/cfba22703aaf79956d6efbb211ad05df.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/e419f7861fe86424820f8e7ac4261b83.jpg"
         ]);
+});
+
+test("article.ksn?articleNo=2252209", async () => {
+    await testDirectDownload(
+        getBrowser(),
+        "https://kstyle.com/article.ksn?articleNo=2252209",
+        "kstyle.com-article.ksn-articleNo-2252209/",
+        [
+            "https://cdn.livedoor.jp/kstyle/kstyle/6c1812a3c02b300d9fa36c8efed31f35.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/c1a8077a213f5609095eaae40ab04dfc.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/f7d621485a1655ee0fdbc6835c7eb6fa.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/00aa2026722fb052f935f1685f58a8ee.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/f49ddc3348a1bdbbd3c15514b16cb514.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/79468137c1e7fb2a28a41307f5368ae9.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/71c17791a6963f80df48a87fab813221.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/05c177f141aa83fd7e99d7cc5331821b.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/d59cfe72dd545f03657d9b61c5eba043.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/6bff70629dd3446fa43f00141dee8874.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/e357c35438d16b818a773b8677650289.jpg",
+            "https://cdn.livedoor.jp/kstyle/kstyle/719dde08d1260e3f2b689b19eca0e863.jpg"
+        ]);
+});
+
+test("article.ksn?articleNo=2252016", async () => {
+    await testDirectDownload(
+        getBrowser(),
+        "https://kstyle.com/article.ksn?articleNo=2252016",
+        "kstyle.com-article.ksn-articleNo-2252016/",
+        dummyItems(20)
+    );
 });
