f1b3c4635802af4de137b768858d875e3245f34a
熊猫 <shoushouminami@gmail.com>
2025-03-09 02:08:25 -1000

    fix tests

--- test/inject/www.comic-valkyrie.com.test.js
+++ test/inject/www.comic-valkyrie.com.test.js
@@ -2,12 +2,12 @@ const {testDirectDownload, getBrowserFactory, dummyItems} = require("./testbase"
 const pageutils = require("../pageutils");
 const getBrowser = getBrowserFactory(beforeAll, afterAll);
 
-test("Test samplebook page", async () => {
+test("samplebook/val_classmate71/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://www.comic-valkyrie.com/samplebook/val_bomber03/",
-        "www.comic-valkyrie.com-samplebook-val_bomber03-/",
-        dummyItems(34),
+        "https://www.comic-valkyrie.com/samplebook/val_classmate71/",
+        "www.comic-valkyrie.com-samplebook-val_classmate71-/",
+        dummyItems(23),
         {
             pagetimeout: 20000,
             preinject: async function () {
