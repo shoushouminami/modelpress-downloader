ca168b1719b00c4330126b0596f24809112ae86a
熊猫 <shoushouminami@gmail.com>
2025-04-10 17:53:17 -1000

    fix on moviewalker.jp

--- src/inject/moviewalker.jp.js
+++ src/inject/moviewalker.jp.js
@@ -14,49 +14,24 @@ const getLargeImg = function (url) {
 module.exports = {
     inject: function () {
         let o = require("./return-message.js").init();
-        // article top image
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(document,
-                [
-
-                    "main .ly_content_main_inner .un_newsDetail_head img",
-                ].join(","),
-                getLargeImg)
-        );
-
-        // in article images
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(document,
-                [
-
-                    "main .ly_content_main_inner .un_newsDetail_content figure a img",
-                ].join(","),
-                getLargeImg)
-        );
-
-        // article end thumbnails
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(document,
-                [
-
-                    "main .un_newsDetail_pic .un_newsDetail_picListItem a img",
-                ].join(","),
-                getLargeImg)
-        );
-
-        // image gallery page
-        utils.pushArray(o.images,
-            utils.findImagesWithCssSelector(document,
-                [
-
-                    "main .un_newsImages_tmb li a img",
-                ].join(","),
-                getLargeImg)
-        );
+        for (const selector of [
+            "main .ly_content_main_inner .un_newsDetail_head img", // article top image
+            "main .ly_content_main_inner .un_newsDetail_content figure a img",  // in article images
+            "main .un_newsDetail_pic .un_newsDetail_picListItem a img", // article end thumbnails
+            "main .un_newsImages_tmb li a img", // image gallery page
+        ]) {
+            utils.pushArray(o.images,
+                utils.findLazyImagesWithCssSelector(
+                    document,
+                    selector,
+                    getLargeImg)
+            );
+        }
 
         o.ext = "jpg";
         return o;
     },
     host: "moviewalker.jp",
+    altHosts: ["press.moviewalker.jp"],
     getLargeImg: getLargeImg
 };
