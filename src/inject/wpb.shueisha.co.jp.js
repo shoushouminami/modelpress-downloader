3a253ec4025f97ce88f0af1ce7c73e573ebeb4da
熊猫 <shoushouminami@gmail.com>
2025-12-15 23:26:18 -1000

    Fix wpb.shueisha.co.jp

--- src/inject/wpb.shueisha.co.jp.js
+++ src/inject/wpb.shueisha.co.jp.js
@@ -1,5 +1,5 @@
 const utils = require("../utils.js");
-const {toFull} = require("../utils/url-utils");
+const { toFull, filters } = require("../utils/url-utils");
 
 function getLargeImg(url) {
     if (url.indexOf("4375d21660a17e6b0e50d5402d16cb885ab42bd1.jpg") > 1 || url.indexOf(".svg") > -1) {
@@ -11,13 +11,34 @@ function getLargeImg(url) {
 module.exports = {
     inject: function () {
         let o = require("./return-message.js").init();
-        for (const selector of ["main .block-detail-a-a__body img", // article image
+        for (const selector of [
+            "main .block-detail-a-a__body img", // article image
             "main .block-detail-a-d__body img", // article image
+            "main .body-entry-a__document img", // article image
             "main .block-layout-c-a__container img", // comic article image
             "main .block-carousel-a-b__list__item img", // article gallery image
         ]) {
             utils.pushArray(o.images, utils.findLazyImagesWithCssSelector(document, selector, getLargeImg));
         }
+
+        // lookup <source> DOM
+        for (const selector of [
+            "main .body-gravure-a__pict source", // article gallery image
+            "main .carousel-gallery-b .carousel-gallery-b__list .carousel-gallery-b__list__item source", // article gallery image
+            "main .body-comic-a__pict source", // comic article top image
+        ]) {
+            utils.pushArray(o.images,
+                utils.findDOMsWithCssSelector(
+                    document,
+                    selector,
+                    filters.chain(
+                        utils.getLazyImageFromDOM, 
+                        toFull
+                    )
+                )
+            );
+        }
+
         return o;
     }, host: "wpb.shueisha.co.jp"
 };
--- test/inject/wpb.shueisha.co.jp.test.js
+++ test/inject/wpb.shueisha.co.jp.test.js
@@ -1,108 +1,116 @@
 const {testDirectDownload, getBrowserFactory} = require("./testbase");
 const getBrowser = getBrowserFactory(beforeAll, afterAll);
-const pageutils = require("../pageutils");
 
-test("news/info/2024/10/15/124824/", async () => {
+test("gravure/info/20241015-124824/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/news/info/2024/10/15/124824/",
-        "wpb.shueisha.co.jp-news-info-2024-10-15-124824-/",
+        "https://wpb.shueisha.co.jp/gravure/info/20241015-124824/",
+        "wpb.shueisha.co.jp-gravure-info-20241015-124824-/",
         [
-            "https://wpb.shueisha.co.jp/news/info/images/WPB_tanaka001.jpg",
-            "https://wpb.shueisha.co.jp/news/info/images/WPB_tanaka002.jpg",
-            "https://wpb.shueisha.co.jp/news/info/images/WPB_tanaka003.jpg",
-            "https://wpb.shueisha.co.jp/news/info/images/WPB_tanaka004.jpg",
-            "https://wpb.shueisha.co.jp/news/info/images/WPB_tanaka005.jpg",
-            "https://wpb.shueisha.co.jp/news/info/images/WPB_tanaka_bookcover.jpg"
+            "https://wpb.shueisha.co.jp/gravure/info/images/WPB_tanaka001.jpg",
+            "https://wpb.shueisha.co.jp/gravure/info/images/WPB_tanaka002.jpg",
+            "https://wpb.shueisha.co.jp/gravure/info/images/WPB_tanaka003.jpg",
+            "https://wpb.shueisha.co.jp/gravure/info/images/WPB_tanaka004.jpg",
+            "https://wpb.shueisha.co.jp/gravure/info/images/WPB_tanaka005.jpg",
+            "https://wpb.shueisha.co.jp/gravure/info/images/WPB_tanaka_bookcover.jpg"
         ]);
 });
 
-test("news/entertainment/2024/10/06/124747/#goog_rewarded", async () => {
+test("gravure/news/20241006-124747/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/news/entertainment/2024/10/06/124747/#goog_rewarded",
-        "wpb.shueisha.co.jp-news-entertainment-2024-10-06-124747-/",
+        "https://wpb.shueisha.co.jp/gravure/news/20241006-124747/",
+        "wpb.shueisha.co.jp-gravure-news-20241006-124747-/",
         [
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/habu01.jpg",
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/1habumizuho_039.jpg",
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/habumizuho_001.jpg"
-        ],
-        {
-            preinject: async function (page) {
-                await pageutils.scrollTo(page, 60);
-            }
-        });
-});
-
-test("news/entertainment/2024/08/18/124146/", async () => {
-    await testDirectDownload(
-        getBrowser(),
-        "https://wpb.shueisha.co.jp/news/entertainment/2024/08/18/124146/",
-        "wpb.shueisha.co.jp-news-entertainment-2024-08-18-124146-/",
-        [
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/%E4%BA%94%E7%99%BE%E5%9F%8E01.jpg"
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/habu01.jpg",
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/1habumizuho_039.jpg",
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/habumizuho_001.jpg"
         ]);
 });
 
-test("news/entertainment/2024/08/18/124173/", async () => {
+test("gravure/news/20240818-124146/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/news/entertainment/2024/08/18/124173/",
-        "wpb.shueisha.co.jp-news-entertainment-2024-08-18-124173-/",
+        "https://wpb.shueisha.co.jp/gravure/news/20240818-124146/",
+        "wpb.shueisha.co.jp-gravure-news-20240818-124146-/",
         [
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/a17348aacd49c93a1bc40ab16a5378403beffbf2.jpg",
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/tomisato02.jpg"
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/%E4%BA%94%E7%99%BE%E5%9F%8E01.jpg"
         ]);
 });
 
-test("news/photo/entertainment/2024/08/18/124173/#gallery-item-1", async () => {
+test("gravure/news/20240818-124173/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/news/photo/entertainment/2024/08/18/124173/#gallery-item-1",
-        "wpb.shueisha.co.jp-news-photo-entertainment-2024-08-18-124173-/",
+        "https://wpb.shueisha.co.jp/gravure/news/20240818-124173/",
+        "wpb.shueisha.co.jp-gravure-news-20240818-124173-/",
         [
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/927f28818609910c654896c4b0365c589ea656af.jpg",
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/a1943283e2943aaa6ff689a247abb0a49406f5d1.jpg"
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/a17348aacd49c93a1bc40ab16a5378403beffbf2.jpg",
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/tomisato02.jpg",
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/927f28818609910c654896c4b0365c589ea656af.jpg",
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/a1943283e2943aaa6ff689a247abb0a49406f5d1.jpg"
         ]);
 });
 
-test("news/entertainment/2024/08/18/124145/", async () => {
+test("gravure/news/20240818-124173/photo/#gallery-item-1", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/news/entertainment/2024/08/18/124145/",
-        "wpb.shueisha.co.jp-news-entertainment-2024-08-18-124145-/",
+        "https://wpb.shueisha.co.jp/gravure/news/20240818-124173/photo/#gallery-item-1",
+        "wpb.shueisha.co.jp-gravure-news-20240818-124173-photo-/",
         [
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/ca7c9d208f22e5496582886563ed068434b831bb.jpg"
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/927f28818609910c654896c4b0365c589ea656af.jpg",
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/a1943283e2943aaa6ff689a247abb0a49406f5d1.jpg"
         ]);
 });
 
-test("news/entertainment/2024/04/01/122716/", async () => {
+test("gravure/news/20240818-124145/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/news/entertainment/2024/04/01/122716/",
-        "wpb.shueisha.co.jp-news-entertainment-2024-04-01-122716-/",
+        "https://wpb.shueisha.co.jp/gravure/news/20240818-124145/",
+        "wpb.shueisha.co.jp-gravure-news-20240818-124145-/",
         [
-            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/9fab274a437fd0bf59d53bf6ac4dd24f80e404fb.jpg"
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/ca7c9d208f22e5496582886563ed068434b831bb.jpg"
         ]);
 });
 
-test("movie/2024/10/13/124791/", async () => {
+test("gravure/news/20240401-122716/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/movie/2024/10/13/124791/",
-        "wpb.shueisha.co.jp-movie-2024-10-13-124791-/",
+        "https://wpb.shueisha.co.jp/gravure/news/20240401-122716/",
+        "wpb.shueisha.co.jp-gravure-news-20240401-122716-/",
         [
-            "https://wpb.shueisha.co.jp/movie/2024/images/1562cbf025ab6c10858238d942584e0a_l.jpg"
+            "https://wpb.shueisha.co.jp/gravure/news/2024/images/9fab274a437fd0bf59d53bf6ac4dd24f80e404fb.jpg"
         ]);
 });
 
-test("comic/2024/10/12/124781/", async () => {
+test("gravure/movie/20241013-124791/", async () => {
     await testDirectDownload(
         getBrowser(),
-        "https://wpb.shueisha.co.jp/comic/2024/10/12/124781/",
-        "wpb.shueisha.co.jp-comic-2024-10-12-124781-/",
+        "https://wpb.shueisha.co.jp/gravure/movie/20241013-124791/",
+        "wpb.shueisha.co.jp-gravure-movie-20241013-124791-/",
         [
-            "https://wpb.shueisha.co.jp/comic/assets_c/2024/09/hotta-evolution_600x360-fs8-thumb-640xauto-227152.png",
-            "https://wpb.shueisha.co.jp/comic/assets_c/2024/10/hotta003_02-3-thumb-274xauto-230350.jpg"
+            "https://wpb.shueisha.co.jp/gravure/movie/2024/images/1562cbf025ab6c10858238d942584e0a_l.jpg"
+        ]);
+});
+
+test("comic/kinnikuman2/20251214-129409/", async () => {
+    await testDirectDownload(
+        getBrowser(),
+        "https://wpb.shueisha.co.jp/comic/kinnikuman2/20251214-129409/",
+        "wpb.shueisha.co.jp-comic-kinnikuman2-20251214-129409-/",
+        [
+            "https://wpb.shueisha.co.jp/news/entertainment/2025/images/15e3413e83d4b0336cc8722c3a6659bc55406b79.jpg",
+            "https://wpb.shueisha.co.jp/comic/assets_c/2025/08/Seaso3-thumb-autox467-261081.jpg",
+            "https://wpb.shueisha.co.jp/news/entertainment/2024/images/7b0e2fbdf23622b6087ef405910e09780f372358.jpg",
+            "https://wpb.shueisha.co.jp/comic/images/kin515_02-3.jpg"
+        ]);
+});
+
+test("gravure/20251130-129312/", async () => {
+    await testDirectDownload(
+        getBrowser(),
+        "https://wpb.shueisha.co.jp/gravure/20251130-129312/",
+        "wpb.shueisha.co.jp-gravure-20251130-129312-/",
+        [
+            "https://wpb.shueisha.co.jp/gravure/2025/images/304d280dfb6dc238c113e7959a1ab24d7cfd8128.jpg"
         ]);
 });
